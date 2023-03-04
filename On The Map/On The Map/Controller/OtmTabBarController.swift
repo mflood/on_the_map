10274b775c86aa4a6ca7e02f3df64e1973757dd9
mflood <hermannwest@gmail.com>
2023-03-04 09:31:45 -0700

    Add map view

---
+++ On The Map/On The Map/Controller/OtmTabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  OtmTabBarController.swift
+//  On The Map
+//
+//  Created by Matthew Flood on 3/4/23.
+//
+
+import Foundation
+import UIKit
+
+class OtmTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
