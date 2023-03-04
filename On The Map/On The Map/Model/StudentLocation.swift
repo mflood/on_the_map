30fd11e703ac4df0a7e5ae5f2c01b7e8e83f143a
mflood <hermannwest@gmail.com>
2023-03-04 09:44:56 -0700

    Add CodingKeys to model

--- On The Map/On The Map/Model/StudentLocation.swift
+++ On The Map/On The Map/Model/StudentLocation.swift
@@ -8,18 +8,31 @@
 import Foundation
 
 class StudentLocation: Codable {
+    
     var objectId: String    // uniquely identifies a StudentLocation (ZExGR5uX8)
     var uniqueKey: String?  // populate this value using your Udacity account id (1234)
     var firstName: String
     var lastName: String
     var mapString: String   // (Mountain View, CA)
-    var mediaURL: String    // the URL provided by the student (https://udacity.com)
+    var mediaUrl: String    // the URL provided by the student (https://udacity.com)
     var latitude: Int       // ranges from -90 to 90 (37.386052)
     var longitude: Int      // ranges from -180 to 180 (-122.083851)
     var createdAt: String
     var updatedAt: String
-    var ACL: String // the Parse access and control list (Public Read and Write)
+    var Acl: String // the Parse access and control list (Public Read and Write)
     
     
-
+    enum CodingKeys: String, CodingKey {
+        case objectId
+        case uniqueKey
+        case firstName
+        case lastName
+        case mapString
+        case mediaUrl = "mediaURL"
+        case latitude
+        case longitude
+        case createdAt
+        case updatedAt
+        case Acl = "ACL"
+    }
 }
