ca3233d014278861d93a085ccf5cdb03b0ac6f66
mflood <hermannwest@gmail.com>
2023-03-05 12:54:59 -0700

    Populate table cells

--- On The Map/On The Map/Controller/OtmTableViewController.swift
+++ On The Map/On The Map/Controller/OtmTableViewController.swift
@@ -11,17 +11,51 @@ import Foundation
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class OtmTableViewController: UITableViewController {
 
+    var studentLocations: [StudentLocation]! {
+        let object = UIApplication.shared.delegate
+        let appDelegate = object as! AppDelegate
+        return appDelegate.studentLocations
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
     }
-
-
-    @IBAction func testMapApi(_ sender: Any) {
-       
+    
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "OtmTableViewCell")!
+        
+        
+        let studentLocation = self.studentLocations[(indexPath as NSIndexPath).row]
+        
+        // Set the name and image
+        cell.textLabel?.text = "\(studentLocation.firstName) \(studentLocation.lastName)"
+       // cell.imageView?.image = meme.memedImage
+        
+        
+        
+        // If the cell has a detail label, we will put the evil scheme in.
+        if let detailTextLabel = cell.detailTextLabel {
+            detailTextLabel.text = studentLocation.mediaUrl
+        }
+        
+        return cell
     }
 
 }
+
 
