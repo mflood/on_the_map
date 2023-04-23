14a644f908366ac0f0fd0d8ecf1af0d044c228b9
mflood <hermannwest@gmail.com>
2023-04-23 12:31:29 -0600

    Add missing call to super()

--- On The Map/On The Map/Controller/OtmTableViewController.swift
+++ On The Map/On The Map/Controller/OtmTableViewController.swift
@@ -31,6 +31,7 @@ class OtmTableViewController: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         self.peopleTableView.reloadData()
     }
 }
